c2d42251475a7eb1cd8b81f15ae37acfd6512c50
Matija Brnas <mb8828@rit.edu>
2023-06-08 17:02:26 +0200

    bootstrap validation done

--- src/components/FlightInput.js
+++ src/components/FlightInput.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, Button } from "react-bootstrap";
 import "../styles/FlightInput.css";
 
 const FlightInput = () => {
   const [submitted, setSubmitted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmitButton = (event) => {
     event.preventDefault();
+
+    // Get form field values
+    const nameInput = document.getElementById("nameInput");
+    const emailInput = document.getElementById("exampleInputEmail1");
+    const destinationInput = document.getElementById("destinationInput");
+    const fromInput = document.getElementById("fromInput");
+
+    // Validate form fields
+    if (
+      nameInput.value === "" ||
+      emailInput.value === "" ||
+      destinationInput.value === "" ||
+      fromInput.value === ""
+    ) {
+      // Display error modal
+      setShowModal(true);
+      return;
+    }
+
+    // Form submission successful
     setSubmitted(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   return (
@@ -22,6 +48,7 @@ const FlightInput = () => {
                 className="form-control input-field"
                 id="nameInput"
                 placeholder="Jane Doe"
+                required
               />
             </div>
             <div className="col">
@@ -32,6 +59,7 @@ const FlightInput = () => {
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
+                required
               />
               <small id="emailHelp" className="form-text text-muted">
                 We'll never share your email with anyone else.
@@ -46,6 +74,7 @@ const FlightInput = () => {
                 className="form-control input-field"
                 id="destinationInput"
                 placeholder="e.g Paris"
+                required
               />
             </div>
             <div className="col">
@@ -55,6 +84,7 @@ const FlightInput = () => {
                 className="form-control input-field"
                 id="fromInput"
                 placeholder="e.g Vienna"
+                required
               />
             </div>
           </div>
@@ -73,6 +103,18 @@ const FlightInput = () => {
       ) : (
         <h3>Form submitted! Thank you</h3>
       )}
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Please fill out all fields before submitting the form.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
