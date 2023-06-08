291bd837baad42e80acee02fe4ef597ab0ee2d90
Matija Brnas <mb8828@rit.edu>
2023-06-08 16:16:01 +0200

    added field for email

--- src/components/FlightInput.js
+++ src/components/FlightInput.js
@@ -5,21 +5,45 @@ const FlightInput = () => {
   return (
     <div className="container d-flex align-items-center justify-content-center">
       <form>
-        <div className="form-group mb-3">
+        <div className="form-group mb-4">
           <label htmlFor="nameInput">Your Name:</label>
-          <input type="text" className="form-control input-field" id="nameInput" />
+          <input
+            type="text"
+            className="form-control input-field"
+            id="nameInput"
+            placeholder="Jane Doe"
+          />
         </div>
-        <div className="form-group mb-3">
+        <div class="form-group mb-4">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group mb-4">
           <label htmlFor="destinationInput">Destination:</label>
           <input
             type="text"
             className="form-control input-field"
             id="destinationInput"
+            placeholder="e.g Paris"
           />
         </div>
-        <div className="form-group mb-3">
+        <div className="form-group mb-4">
           <label htmlFor="fromInput">From:</label>
-          <input type="text" className="form-control input-field" id="fromInput" />
+          <input
+            type="text"
+            className="form-control input-field"
+            id="fromInput"
+            placeholder="e.g Vienna"
+          />
         </div>
       </form>
     </div>
