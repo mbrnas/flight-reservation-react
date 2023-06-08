d7b1b5686c509facaa8ff148b3d10bc1d07e7a4e
Matija Brnas <mb8828@rit.edu>
2023-06-08 12:06:39 +0200

    added bootstrap styling

--- src/FlightInput.js
+++ src/FlightInput.js
@@ -1,3 +1,24 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const FlightInput = () => {
+  return (
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="nameInput">Your Name:</label>
+          <input type="text" className="form-control" id="nameInput" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="destinationInput">Destination:</label>
+          <input type="text" className="form-control" id="destinationInput" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="fromInput">From:</label>
+          <input type="text" className="form-control" id="fromInput" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-const FlightInput = () => {};
+export default FlightInput;
